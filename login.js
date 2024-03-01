36cba630bb0e117a645c014dfdafdcfae8086b9a
“Zach <zacharyyork0@gmail.com>
2024-03-01 13:31:17 -0700

    Added logic for password when log in and in future will add more logic in the databse to check password

--- login.js
+++ login.js
@@ -1,5 +1,12 @@
 function login() {
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "calender.html";
-  }
+  const nameEl = document.querySelector("#name");
+  const passwordEl = document.querySelector("#password");
+
+  // Here I can add logic to check if the password matches the username in the database
+  const users = JSON.parse(localStorage.getItem("users")) || {};
+  users[nameEl.value] = passwordEl.value;
+  localStorage.setItem("users", JSON.stringify(users));
+
+  // Redirect to calendar.html
+  window.location.href = "calendar.html";
+}
