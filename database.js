6d58304d41e7228f7e2584c3166dd3771ccdf3d0
“Zach <zacharyyork0@gmail.com>
2024-03-18 14:01:25 -0600

    doing database initials

---
+++ database.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+async function main() {
+
+    const uri = "mongodb+srv://cs260:startup@cluster0.ioeicrv.mongodb.net/?";
+    
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+// Add functions that make DB calls here
